1871fdec2fcaadd715e6bd8cb888b933a6645041
crass_sandwich <christopherjanidlo@gmail.com>
2019-06-12 02:59:46 -0400

    Text design for design screen

---
+++ Assets/Scripts/TextRandomizer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using crass;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class TextRandomizer : MonoBehaviour
+{
+    [Serializable]
+    public class SizedFont
+    {
+        public TMP_FontAsset Font;
+        public float Size;
+    }
+
+    [Serializable]
+    public class FontBag : BagRandomizer<SizedFont> {}
+
+    public FontBag Fonts;
+    public Vector2 FontSwapTimeRange;
+
+    IEnumerator Start ()
+    {
+        var text = GetComponent<TextMeshProUGUI>();
+        
+        while (true)
+        {
+            var next = Fonts.GetNext();
+            
+            text.font = next.Font;
+            text.fontSize = next.Size;
+
+            yield return new WaitForSeconds(RandomExtra.Range(FontSwapTimeRange));
+        }
+    }
+}
