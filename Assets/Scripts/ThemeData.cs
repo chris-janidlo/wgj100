f45400c09d0bce13ec66c3e390a7ae58b655a695
crass_sandwich <christopherjanidlo@gmail.com>
2019-06-12 15:16:48 -0400

    Make ThemeData structure, starting theme data

---
+++ Assets/Scripts/ThemeData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class ThemeData
+{
+    [Serializable]
+    public class Theme
+    {
+        public string Name;
+        public List<ScoredGenre> GenresAndScores;
+    }
+
+    [Serializable]
+    public class Genre
+    {
+        public string Name;
+        public BlockType IncreaseType1, IncreaseType2;
+        public int IncreaseAmount1, IncreaseAmount2;
+    }
+
+    [Serializable]
+    public class ScoredGenre
+    {
+        public string Genre;
+        public float Score;
+    }
+
+    public List<Theme> Themes;
+    public List<Genre> Genres;
+}
