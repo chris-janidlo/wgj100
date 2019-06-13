ed17a37a432083c46e85efa246a5be2c40c15fac
crass_sandwich <christopherjanidlo@gmail.com>
2019-06-13 11:34:45 -0400

    Timer, controls prompt, and repositioning

---
+++ Assets/Scripts/CreationTimer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class CreationTimer : MonoBehaviour
+{
+    public float LevelTime;
+    public TextMeshProUGUI Text;
+
+    void Update ()
+    {
+        Text.text = Mathf.CeilToInt(LevelTime).ToString();
+
+        LevelTime -= Time.deltaTime;
+        if (LevelTime <= 0)
+        {
+            SceneManager.LoadScene("PostCreation");
+        }
+    }
+}
